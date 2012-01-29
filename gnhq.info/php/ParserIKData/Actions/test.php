c65d04c59d1ebb20497a8b3e8a5254daf64693fe
gnhq <nv@gnhq.info>
2012-01-29 11:25:21 +0300

    test

--- gnhq.info/php/ParserIKData/Actions/test.php
+++ gnhq.info/php/ParserIKData/Actions/test.php
@@ -7,9 +7,10 @@ $resultType = 'GN';
 * @var ParserIKData_Warehouse_Interface
 */
 $warehouse = ParserIKData_ServiceLocator::getInstance()->getWarehouse();
-$warehouse->loadElectionResults($electionCode, 'GN')->loadElectionResults($electionCode, 'OF');
+$warehouse
+    ->loadAllOkrugs()
+    ->loadAllOkrugs()
+    ->loadAllUiks()
+    ->loadElectionResults($electionCode, 'GN')
+    ->loadElectionResults($electionCode, 'OF');
 
-$of139 = ParserIKData_Model_Protocol412::getFromPool('139:UIK:OF');
-$gn139 = ParserIKData_Model_Protocol412::getFromPool('139:UIK:GN');
-var_dump($of139);
-var_dump($gn139);

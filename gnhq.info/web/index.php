2bf56582235f6b6c5f650d162dc3860e3157a12e
gnhq <nv@gnhq.info>
2012-02-11 22:51:57 +0300

    авторизация - в игнор

--- gnhq.info/web/index.php
+++ gnhq.info/web/index.php
@@ -13,15 +13,15 @@ $view->totalUikCount = $watchGateway->getCount(WATCH_GN);
 $view->discrepancyUikCount = $watchGateway->getCount(WATCH_GN, null, true);
 
 $view->diagRows = array(
-	array('key' => 'S',  'hdr' => 'СР',     'title' => 'Справедливая Россия', 'color' => 'yellow'),
-	array('key' => 'L',  'hdr' => 'ЛДПР',   'title' => 'Либерально-Демократическая Партия России', 'color' => 'orange'),
-	array('key' => 'PR', 'hdr' => 'ПР',     'title' => 'Патриоты России', 'color' => '#ff77ff'),
-	array('key' => 'K',  'hdr' => 'КПРФ',   'title' => 'Коммунистичесая Партия Российской Федерации', 'color' => 'red'),
-	array('key' => 'Y',  'hdr' => 'Яблоко', 'title' => 'Яблоко', 'color' => '#66ee00'),
-	array('key' => 'E',  'hdr' => 'ЕР',     'title' => 'Единая Россия', 'color' => '#004466'),
-	array('key' => 'PD', 'hdr' => 'ПД',     'title' => 'Правое Дело', 'color' => '#66ffff'),
-	array('key' => 'AT', 'hdr' => 'Явка', 'color' => 'magenta'),
-	array('key' => 'SP', 'hdr' => 'Недействительные', 'color' => 'grey'),
+	array('key' => 'S',  'hdr' => 'СР',               'title' => 'Справедливая Россия', 'color' => 'yellow'),
+	array('key' => 'L',  'hdr' => 'ЛДПР',             'title' => 'Либерально-Демократическая Партия России', 'color' => 'orange'),
+	array('key' => 'PR', 'hdr' => 'ПР',               'title' => 'Патриоты России', 'color' => '#ff77ff'),
+	array('key' => 'K',  'hdr' => 'КПРФ',             'title' => 'Коммунистичесая Партия Российской Федерации', 'color' => 'red'),
+	array('key' => 'Y',  'hdr' => 'Яблоко',           'title' => 'Яблоко', 'color' => '#66ee00'),
+	array('key' => 'E',  'hdr' => 'ЕР',               'title' => 'Единая Россия', 'color' => '#004466'),
+	array('key' => 'PD', 'hdr' => 'ПД',               'title' => 'Правое Дело', 'color' => '#66ffff'),
+	array('key' => 'AT', 'hdr' => 'Явка',             'title' => 'Явка', 'color' => 'magenta'),
+	array('key' => 'SP', 'hdr' => 'Недействительные', 'title' => 'Недействительные', 'color' => 'grey'),
 );
 
 include 'tpl/index.tpl';

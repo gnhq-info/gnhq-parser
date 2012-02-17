afeeb28f1561acf0518e85086f3c7ec540d22c98
gnhq <nv@gnhq.info>
2012-02-17 23:22:09 +0300

    zend standalone

--- gnhq.info/php/Lib/ZendConfig.php
+++ gnhq.info/php/Lib/ZendConfig.php
@@ -1,3 +1,3 @@
 <?php
-const PATH_TO_ZEND = 'C:\var\www\zend\ZendFramework-1.11.11-minimal\library';
+const PATH_TO_ZEND = '\\var\\www\\zend\\Zend-standalone';
 ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . PATH_TO_ZEND);

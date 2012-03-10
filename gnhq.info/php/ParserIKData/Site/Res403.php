14597b6e40bd55d8758ad15a3ea29739fb7a38c3
gnhq <nv@gnhq.info>
2012-03-10 21:25:33 +0300

    cleaning

--- gnhq.info/php/ParserIKData/Site/Res403.php
+++ gnhq.info/php/ParserIKData/Site/Res403.php
@@ -38,7 +38,7 @@ class ParserIKData_Site_Res403 extends ParserIKData_Site_Abstract
     /**
      * @param ParserIKData_Model_UIKRussia $uik
      * @param string $link
-     * @return Ambigous <string, false, boolean>
+     * @return ParserIKData_Model_Protocol403
      */
     public function getProtocol($uik, $link)
     {
@@ -64,83 +64,6 @@ class ParserIKData_Site_Res403 extends ParserIKData_Site_Abstract
         return $proto;
     }
 
-    /**
-     * @param html $table
-     */
-    public function getResultsFromTable($table)
-    {
-        $cells = array();
-        $rows = $this->_getMiner()->extractRows($table, 50);
-        foreach ($rows as $k => $row) {
-            $rowCells = $this->_getMiner()->extractCells($row, 100);
-            foreach ($rowCells as $j => $cell) {
-                if ($k == 0) {
-                    $rowCells[$j] = $this->_prepareHeaderCellValue($cell);
-                } else {
-                    $rowCells[$j] = $this->_prepareCellValue($cell, true);
-                }
-            }
-            $cells[$k] = $rowCells;
-        }
-        $cells = $this->_transoneTable($cells);
-        $results = array();
-        foreach ($cells as $k => $row) {
-            $name = array_shift($row);
-            $i = 1;
-            foreach ($row as $j => $v) {
-                $results[$name][$i] = $v;
-                $i++;
-            }
-        }
-        return $results;
-    }
-
-
-    /**
-     * @param string $uikName
-     * @param array $data
-     * @return ParserIKData_Model_Protocol412
-     */
-    public function createResult($uikName, $data)
-    {
-        $uikNumber = str_replace('УИК №', '', $uikName);
-        $result = ParserIKData_Model_Protocol412::createFromPageInfo($uikNumber, '', array());
-        /* @var $result ParserIKData_Model_Protocol412 */
-        $result->setTypeOf();
-        $result->setData($data);
-        $result->setIkTypeUIK();
-        return $result;
-    }
-
-
-    /**
-     * @param string[][] $cells
-     * @return string[][]
-     */
-    private function _transoneTable($cells)
-    {
-        $results = array();
-        foreach ($cells as $k => $row) {
-            foreach ($row as $j => $cell) {
-                if ($cell !== '') {
-                    $results[$j][$k] = $cell;
-                }
-            }
-        }
-        return $results;
-    }
-
-    /**
-     * @param html $cell
-     * @return Ambigous <multitype:Ambigous, multitype:Ambigous <string, false, boolean> >
-     */
-    private function _prepareHeaderCellValue($cell)
-    {
-        $nobrs = $this->_getMiner()->extractNobr($cell, 5);
-        $nobr = $nobrs[0];
-        $nobr = strip_tags($nobr);
-        return $nobr;
-    }
 
     /**
      * @param html $cell
@@ -163,47 +86,5 @@ class ParserIKData_Site_Res403 extends ParserIKData_Site_Abstract
         } else {
             return array('numeric' => $numericValue, 'percent' => $percentValue);
         }
-    }
-
-    /**
-     * @param string $page
-     * @return Ambigous <mixed, multitype:, multitype:Ambigous <Ambigous <string, false, boolean>> >
-     */
-    private function _getLowerLinks($page)
-    {
-        $page = $this->_getPageContent($page, $this->_getCValue('useCache'));
-        $select = $this->_lowerIKsSelect($page);
-        $options = $this->_getMiner()->extractOptions($select, 50);
-        $optionData = $this->_getMiner()->getOptions($options);
-        foreach ($optionData as $okrug => $link) {
-            if ($link != '') {
-                $optionData[$okrug] = $this->_excludeSiteFromLink(html_entity_decode($link));
-            } else {
-                unset ($optionData[$okrug]);
-            }
-        }
-        return $optionData;
-    }
-
-    /**
-     *
-     * Enter description here ...
-     * @param unknown_type $link
-     * @return mixed
-     */
-    private function _excludeSiteFromLink($link)
-    {
-        return str_replace($this->_getSite(), '', $link);
-    }
-
-    /**
-     * @param string $page
-     * @return Ambigous <string, false, boolean>
-     */
-    private function _lowerIKsSelect($page)
-    {
-        $form = $this->_getParser()->setPageSource($page)->findMinContainingTag($this->_getCValue('lowerIKIndicator'), 'form');
-        $select = $this->_getParser()->setPageSource($form)->findMinContainingTag($this->_getCValue('lowerIKSelectIndicator'), 'select');
-        return $select;
     }
 }

477e1ff968fe76f0b998e32ea70076581e4e30f7
gnhq <nv@gnhq.info>
2012-06-10 23:06:17 +0400

    refactoring

--- gnhq.info/web/elections/krasnoyarsk/index.php
+++ gnhq.info/web/elections/krasnoyarsk/index.php
@@ -1,14 +1,14 @@
 <?php
 $view->diagRows = array(
-array('key' => 'EA',  'hdr' => 'Эдхам Акбулатов',         'title' => 'Эдхам Акбулатов',         'color' => 'yellow'),
-array('key' => 'MI',  'hdr' => 'Максим Иваныч',           'title' => 'Максим Иваныч',           'color' => 'orange'),
-array('key' => 'NK',  'hdr' => 'Нина Коврова',            'title' => 'Нина Коврова',            'color' => '#ff77ff'),
-array('key' => 'AK',  'hdr' => 'Александр Коропачинский', 'title' => 'Александр Коропачинский', 'color' => '#77ffff'),
-array('key' => 'AM',  'hdr' => 'Алексей Мещеряков',       'title' => 'Алексей Мещеряков',       'color' => '#ffff77'),
-array('key' => 'MO',  'hdr' => 'Михаил Осколков',         'title' => 'Михаил Осколков',         'color' => 'red'),
-array('key' => 'AP',  'hdr' => 'Алексей Подкорытов',      'title' => 'Алексей Подкорытов',      'color' => '#66ee00'),
-array('key' => 'AT', 'hdr' => 'Явка',             'title' => 'Явка', 'color' => 'magenta'),
-array('key' => 'SP', 'hdr' => 'Недействительные', 'title' => 'Недействительные', 'color' => 'grey'),
+    array('key' => 'EA', 'hdr' => 'Эдхам Акбулатов',         'title' => 'Эдхам Акбулатов',         'color' => 'yellow'),
+    array('key' => 'MI', 'hdr' => 'Максим Иваныч',           'title' => 'Максим Иваныч',           'color' => 'orange'),
+    array('key' => 'NK', 'hdr' => 'Нина Коврова',            'title' => 'Нина Коврова',            'color' => '#ff77ff'),
+    array('key' => 'AK', 'hdr' => 'Александр Коропачинский', 'title' => 'Александр Коропачинский', 'color' => '#77ffff'),
+    array('key' => 'AM', 'hdr' => 'Алексей Мещеряков',       'title' => 'Алексей Мещеряков',       'color' => '#ffff77'),
+    array('key' => 'MO', 'hdr' => 'Михаил Осколков',         'title' => 'Михаил Осколков',         'color' => 'red'),
+    array('key' => 'AP', 'hdr' => 'Алексей Подкорытов',      'title' => 'Алексей Подкорытов',      'color' => '#66ee00'),
+    array('key' => 'AT', 'hdr' => 'Явка',             		 'title' => 'Явка',                    'color' => 'magenta'),
+    array('key' => 'SP', 'hdr' => 'Недействительные',        'title' => 'Недействительные',        'color' => 'grey'),
 );
 
 $view->electionsName = 'Выборы мэра Красноярска. 10.06.2012';

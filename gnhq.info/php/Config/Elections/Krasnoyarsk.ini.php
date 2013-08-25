653725d9b5e9bb1d2b1df6d1b9b3d984c5f36824
gnhq <nv@gnhq.info>
2013-08-25 22:35:21 +0400

    links for watchers results and violations for krasnoyarsk

--- gnhq.info/php/Config/Elections/Krasnoyarsk.ini.php
+++ gnhq.info/php/Config/Elections/Krasnoyarsk.ini.php
@@ -2,9 +2,11 @@
 $projects = array(
         PROJECT_GN => array(
                 'Name'      => 'Гражданин наблюдатель',
+                'ProtoLink'  => 'https://gnhq.info/comm/protocols.xml'
         ),
         PROJECT_GOLOS => array(
                 'Name'      => 'Голос',
+                'ViolLink'  => 'http://m.kartanarusheniy.org/messages/date/2012-06-10/56.0141053,92.8643491/56.0141053,92.8643491/geo',
         ),
 );
 

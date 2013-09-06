55f3b280d14811d6f922fdf1facb44bd72b0466f
gnhq <nv@gnhq.info>
2013-09-06 23:59:58 +0400

    t

--- gnhq.info/php/ParserIKData/Elections/Mosmer/Gateway/Violation.php
+++ gnhq.info/php/ParserIKData/Elections/Mosmer/Gateway/Violation.php
@@ -9,6 +9,6 @@ class ParserIKData_Gateway_Violation_Mosmer extends ParserIKData_Gateway_Violati
     */
     protected function _getCacheLifetime()
     {
-        return 1;
+        return 120;
     }
 }
